c33dcc1af3cfb0a879198f85e208e3418abbe45d
devinlin2022 <112993366+devinlin2022@users.noreply.github.com>
2025-07-14 14:48:07 +0800

    Update sheet_automation.py

--- sheet_automation.py
+++ sheet_automation.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
-import pymupdf
+# import pymupdf
 import pygsheets
 from gspread_dataframe import set_with_dataframe
 import gspread
