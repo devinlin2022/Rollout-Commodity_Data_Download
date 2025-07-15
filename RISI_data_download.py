4b1282419c2cd4a7f8c8018734b673fb53474940
devinlin2022 <112993366+devinlin2022@users.noreply.github.com>
2025-07-15 13:27:14 +0800

    Update RISI_data_download.py

--- RISI_data_download.py
+++ RISI_data_download.py
@@ -91,9 +91,13 @@ def fetch_RISI_data(link):
         # 2. 点击导出菜单 (您的选择器未变)
         print("等待并点击第一个导出相关按钮...")
         # 等待第一个按钮出现并变为可点击状态
-        first_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '#cells-container > fui-grid-cell > fui-widget > header > fui-widget-actions > div:nth-child(1) > button > span.mat-mdc-button-touch-target')))
-        first_button.click()
         time.sleep(10)
+        wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#cells-container > fui-grid-cell > fui-widget")))
+        print("找到表格")
+        first_button = wait.until(EC.visibility_of_element_located(By.CSS_SELECTOR, '#cells-container > fui-grid-cell > fui-widget > header > fui-widget-actions > div:nth-child(1) > button > span.mat-mdc-button-touch-target'))
+        first_button.click()
+        time.sleep(2)
+        
         # 3. 点击下载 CSV 选项 (您的选择器未变)
         print("等待并点击下载CSV选项...")
         # 等待下载菜单项出现并变为可点击状态
